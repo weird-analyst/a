b844fbe9fa9477beb0c44578ac110a7040c53a76
Adarsh Gupta <iamadarshgupta8@gmail.com>
2023-09-16 12:36:27 +0530

    2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import Head from 'next/head'
 
 export default function Document() {
   return (
