2596641500a3ffd31ade2c9b393474f3838262b9
Adarsh Gupta <iamadarshgupta8@gmail.com>
2023-09-16 19:26:06 +0530

    update meta 2

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
   return (
     <Html lang="en" className='scroll-smooth'>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
-        <meta name="viewport" content="width=1024"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=0.1"></meta>
       <Head />
       <body className='text-black'>
         <NextScript />
