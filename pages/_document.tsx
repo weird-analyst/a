f16fb5aff6a912dbfe25861d8cdf6b7697fbf38e
Adarsh Gupta <133852661+weird-analyst@users.noreply.github.com>
2023-09-16 12:13:44 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
   return (
     <Html lang="en" className='scroll-smooth'>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
-        <meta name="viewport" content="width=1024">
+        <meta name="viewport" content="width=1024" />
       <Head />
       <body className='text-black'>
         <NextScript />
